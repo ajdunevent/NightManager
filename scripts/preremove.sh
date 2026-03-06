601155865329c23119b167020d7093390097ad8e
Sean E. Russell <ser@ser1.net>
2026-03-06 12:31:46 -0800

    Pre/post install script fixing

--- scripts/preremove.sh
+++ scripts/preremove.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-sudo -u furios systemctl --machine=furios@.host --user disable --now nightynight-off.timer nightynight-on.timer
+systemctl --machine=furios@.host --user disable --now nightynight-off.timer nightynight-on.timer
